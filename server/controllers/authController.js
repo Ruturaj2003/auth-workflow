f78827709e1f93b8f9020c30ca4750a334e1c51c
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-12-26 18:57:23 +0530

    Add email verification functionality to authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,6 +33,28 @@ const register = async (req, res) => {
     verificationToken,
   });
 };
+
+// Email Verify
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+
+  user.verificationToken = ""; // Verification link wont work again
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -70,6 +92,7 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
 // const tokenUser = createTokenUser(user);
 // attachCookiesToResponse({ res, user: tokenUser });
