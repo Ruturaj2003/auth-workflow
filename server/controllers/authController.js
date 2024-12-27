a7ecc5766d1e59b243b51e2f5fb2d57da0d8a012
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-12-27 12:49:14 +0530

    Implement forgot password functionality with email validation and token generation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -142,7 +142,24 @@ const logout = async (req, res) => {
 };
 
 const forgotPassword = async (req, res) => {
-  res.send("I forgot");
+  const { email } = req.body;
+  if (!email) {
+    throw new CustomError.BadRequestError("Provide Valid Email");
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString("hex");
+
+    // Send Email
+
+    const tenMin = 1000 * 60 * 10;
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMin);
+
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+  res.status(StatusCodes.OK).json("Please Check Email for reset Passowrd Link");
 };
 
 const resetPassword = async (req, res) => {
